e6b4cf1f4982dec912f07c5ee5d83f2bcd4581e1
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-12-08 18:01:52 +0900

    add minimal example

---
+++ examples/minimal/main.c
@@ -0,0 +1,26 @@
+// #include <string.h>
+#include <stdlib.h>
+#include "../../ui.h"
+
+int onClosing(uiWindow *w, void *data)
+{
+	uiQuit();
+	return 1;
+}
+
+int main(void)
+{
+	uiInitOptions o;
+	uiWindow *w;
+
+	memset(&o, 0, sizeof (uiInitOptions));
+	if (uiInit(&o) != NULL){
+		abort();
+    }
+
+	w = uiNewWindow("Hello", 320, 240, 0);
+	uiWindowOnClosing(w, onClosing, NULL);
+	uiControlShow(uiControl(w));
+	uiMain();
+	return 0;
+}
